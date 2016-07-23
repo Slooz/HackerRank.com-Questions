e4e2c65ac8849a9db5a23edbd1d49fadc90562c0
Krzysztof Czelusniak <krzysztof@czelusniak.com>
2016-07-23 17:20:41 -0500

    Solution to Day 0: Hello, World.

---
+++ Tutorials/Day 0 Hello World/Day 0 Hello World/main.cc
@@ -0,0 +1,11 @@
+// Copyright [2016] <Krzysztof Czelusniak>
+
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string input_string;
+  getline(std::cin, input_string);
+
+  std::cout << "Hello, World.\n" << input_string;
+}
